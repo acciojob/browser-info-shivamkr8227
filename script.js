cb293b1c7df9372e6c7cfcb19e8729667922e62b
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-04-19 15:04:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const browsername=navigator.appName;
+const browerversion=navigator.appversion;
+const browserinfo=document.getElementById("browser-info");
+browserinfo.textContent=`You are using ${browserName} version ${version}.`;
